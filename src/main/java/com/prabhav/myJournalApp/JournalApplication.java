0d1862d270c915838661f4d8d7fe5ede80e2480b
Prabhav Kumar Singh <prabhavsingh404@gmail.com>
2025-11-19 01:01:10 +0530

    Added profiles

--- src/main/java/com/prabhav/myJournalApp/JournalApplication.java
+++ src/main/java/com/prabhav/myJournalApp/JournalApplication.java
@@ -2,12 +2,18 @@ package com.prabhav.myJournalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class JournalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JournalApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JournalApplication.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
     }
 
 
