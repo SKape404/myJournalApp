1ea767d8b50fb0bf5c5c1a975540158019912e36
Prabhav Kumar Singh <prabhavsingh404@gmail.com>
2025-11-20 16:21:39 +0530

    Added Criteria for user in Mongo Repo

---
+++ src/test/java/com/prabhav/myJournalApp/repository/UserRepositoryImplTests.java
@@ -0,0 +1,18 @@
+package com.prabhav.myJournalApp.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryImplTests {
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Test
+    public void saveNewUser(){
+        Assertions.assertNotNull(userRepository.getUserForSA());
+    }
+}
