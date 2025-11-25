1c0382e1d8745a6f9cba4657b908a543a815c37c
Prabhav Kumar Singh <prabhavsingh404@gmail.com>
2025-11-25 18:20:11 +0530

    Added Scheduler

--- src/main/java/com/prabhav/myJournalApp/JournalApplication.java
+++ src/main/java/com/prabhav/myJournalApp/JournalApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class JournalApplication {
 
